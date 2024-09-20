5d89587347719178202fab9badc335c85a11b501
Sourabh Choure <sourabhchoure98@gmail.com>
2024-09-20 16:43:44 +0000

    Add Smiles to sdf

--- utils/rdkit.py
+++ utils/rdkit.py
@@ -5,11 +5,12 @@ from pydantic import BaseModel
 from rdchiral.initialization import rdchiralReactants, rdchiralReaction
 from rdchiral.main import rdchiralRun
 from rdkit import Chem
-from rdkit.Chem import Descriptors, rdDepictor
+from rdkit.Chem import Descriptors, rdDepictor, SDWriter, AllChem
 from typing import Any
 from utils import register_util
 from utils.draw_impl import align_molecule
 from utils.registry import get_util_registry
+from io import StringIO
 
 
 class SmilesInput(BaseModel):
@@ -74,7 +75,9 @@ class RDKitUtil:
         "from_molfile": ["POST"],
         "to_molfile": ["POST"],
         "apply_one_template": ["POST"],
-        "apply_one_template_by_idx": ["POST"]
+        "apply_one_template_by_idx": ["POST"],
+        "smiles_to_sdf": ["POST"],
+        "smiles_order": ["POST"]
     }
 
     def __init__(self, util_config: dict[str, Any] = None):
@@ -306,3 +309,68 @@ class RDKitUtil:
         )
 
         return async_return
+    
+    @staticmethod
+    async def smiles_to_sdf(smiles: str)-> Response:
+        # Convert SMILES to Mol object
+        resp = {}
+        mol = Chem.MolFromSmiles(smiles)
+        if not mol:
+            resp["error"] = "Cannot parse smiles with rdkit."
+
+            return Response(
+                content=json.dumps(resp),
+                status_code=500,
+                media_type="application/json"
+            )
+        
+        # Add explicit hydrogens to the molecule
+        mol_with_hs = Chem.AddHs(mol)
+
+        # Generate 3D coordinates for the molecule
+        AllChem.EmbedMolecule(mol_with_hs)
+        AllChem.UFFOptimizeMolecule(mol_with_hs)
+    
+        # Convert the Mol object with hydrogens to SDF format
+        sdf_buffer = StringIO()
+        writer = SDWriter(sdf_buffer)
+        writer.write(mol_with_hs)
+        writer.close()
+    
+        sdf_content = sdf_buffer.getvalue()
+        sdf_buffer.close()
+
+        resp["sdf"] = sdf_content
+
+        return Response(
+            content=json.dumps(resp),
+            status_code=200,
+            media_type="application/json"
+        )
+    
+    @staticmethod
+    def smiles_order(smiles: str)-> Response:
+        resp = {}
+        mol = Chem.MolFromSmiles(smiles)
+        if not mol:
+            resp["error"] = "Cannot parse smiles with rdkit."
+
+            return Response(
+                content=json.dumps(resp),
+                status_code=500,
+                media_type="application/json"
+            )
+        mol = Chem.AddHs(mol)
+        atoms = mol.GetAtoms()  # the atom targets follow the order of this list of atom objects in RDKit
+        bond = mol.GetBonds()  # the bond targets follow the order of this list of bond objects in RDKit
+        resp["atom"] = atoms
+        resp["bond"] = bond
+
+        return Response(
+            content=json.dumps(resp),
+            status_code=200,
+            media_type="application/json"
+        )
+        
+
+
